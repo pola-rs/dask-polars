575acf1be5760dee856ae39c2e3817a87c3c1fde
Ritchie Vink <ritchie46@gmail.com>
2022-02-13 14:50:18 +0100

    add tooling

--- dask_polars/tests/test_core.py
+++ dask_polars/tests/test_core.py
@@ -1,15 +1,18 @@
-import dask_polars as dp
 import polars
 
+import dask_polars as dp
 
 df = polars.from_dict({"a": [1, 2, 3, 4], "b": [1.0, 2.0, 3.0, 4.0]})
 ddf = dp.from_dataframe(df)
 
+
 def test_basic():
     assert str(ddf.compute()) == str(df)
 
+
 def test_sum():
     assert str(ddf.sum().compute()) == str(df.sum())
+
 
 def test_add():
     assert str((ddf + 2).compute()) == str(df + 2)
